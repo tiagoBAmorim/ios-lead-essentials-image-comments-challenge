9d529ca343d66e242b62edad89de05f0df73743d
Tiago Amorim <tiago.amorim@wit-software.com>
2021-09-02 20:38:46 +0100

    Create map functions in PresenterViewModel

--- EssentialFeed/EssentialFeedTests/Image Comments/Presentation/ImageCommentsPresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/Image Comments/Presentation/ImageCommentsPresenterTests.swift
@@ -11,11 +11,36 @@ class ImageCommentsPresenterTests: XCTestCase {
 	}
 
 	func test_map_createsViewModel() {
-		let feed = uniqueImageFeed().models
+		let now = Date()
+		let calendar = Calendar(identifier: .gregorian)
+		let locale = Locale(identifier: "en_US_POSIX")
 
-		let viewModel = FeedPresenter.map(feed)
+		let comments = [
+			ImageComment(id: UUID(),
+			             message: "a message",
+			             createdAt: now.adding(minutes: -5, calendar: calendar),
+			             username: "a username"),
+			ImageComment(id: UUID(),
+			             message: "another message",
+			             createdAt: now.adding(days: -1, calendar: calendar),
+			             username: "another username")
+		]
 
-		XCTAssertEqual(viewModel.feed, feed)
+		let viewModel = ImageCommentsPresenter.map(comments,
+		                                           currentDate: now,
+		                                           calendar: calendar,
+		                                           locale: locale)
+
+		XCTAssertEqual(viewModel.comments, [
+			ImageCommentViewModel(message: "a message",
+			                      date: "5 minutes ago",
+			                      username: "a username"
+			),
+			ImageCommentViewModel(message: "another message",
+			                      date: "1 day ago",
+			                      username: "another username"
+			)
+		])
 	}
 
 	// MARK: - Helpers
