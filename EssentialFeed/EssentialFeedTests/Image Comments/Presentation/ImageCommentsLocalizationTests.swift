005fb9f457e01f7dd6d383bee770eeebce3434b3
Tiago Amorim <tiago.amorim@wit-software.com>
2021-09-02 20:35:44 +0100

    Make sure that ImageComments strings file supports all localizations

---
+++ EssentialFeed/EssentialFeedTests/Image Comments/Presentation/ImageCommentsLocalizationTests.swift
@@ -0,0 +1,15 @@
+//
+// Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+import XCTest
+import EssentialFeed
+
+class ImageCommentsLocalizationTests: XCTestCase {
+	func test_localizedStrings_haveKeysAndValuesForAllSupportedLocalizations() {
+		let table = "ImageComments"
+		let bundle = Bundle(for: ImageCommentsPresenter.self)
+
+		assertLocalizedKeyAndValuesExist(in: bundle, table)
+	}
+}
